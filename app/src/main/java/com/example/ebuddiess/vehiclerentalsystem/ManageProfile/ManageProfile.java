0d017df1ec0df000342e0e390dfe88d2b1c41485
Puneet Kumar <ebuddiess@gmail.com>
2018-07-09 20:02:16 +0530

    welcome user ui done

---
+++ app/src/main/java/com/example/ebuddiess/vehiclerentalsystem/ManageProfile/ManageProfile.java
@@ -0,0 +1,15 @@
+package com.example.ebuddiess.vehiclerentalsystem.ManageProfile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ebuddiess.vehiclerentalsystem.R;
+
+public class ManageProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_profile);
+    }
+}
--- app/src/main/java/com/example/ebuddiess/vehiclerentalsystem/WelcomeUser/WelcomeUser.java
+++ app/src/main/java/com/example/ebuddiess/vehiclerentalsystem/WelcomeUser/WelcomeUser.java
@@ -5,23 +5,29 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.ebuddiess.vehiclerentalsystem.Authentication.Signin;
+import com.example.ebuddiess.vehiclerentalsystem.ManageProfile.ManageProfile;
 import com.example.ebuddiess.vehiclerentalsystem.R;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class WelcomeUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class WelcomeUser extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 NavigationView user_nav_view;
 FirebaseAuth currentUser;
+CardView manageProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_user);
         user_nav_view = (NavigationView)findViewById(R.id.user_navigation_view);
         currentUser = FirebaseAuth.getInstance();
+        manageProfile = (CardView)findViewById(R.id.manager_profile_cardView);
+        manageProfile.setOnClickListener(this);
         user_nav_view.setNavigationItemSelectedListener(this);
     }
 
@@ -38,5 +44,12 @@ FirebaseAuth currentUser;
         currentUser.signOut();
         finish();
         startActivity(new Intent(WelcomeUser.this, Signin.class));
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.manager_profile_cardView:startActivity(new Intent(WelcomeUser.this,ManageProfile.class));
+        }
     }
 }
